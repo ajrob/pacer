3bb7c3468456178acb4e04cd1d5f6e01ef197bb5
Alex Robinson <ajrob.dev@gmail.com>
2014-04-28 22:35:24 -0600

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('pacerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
